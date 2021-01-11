875de049759f18a00ed89291fcb133e4f00bf1b1
SnazzyDucc <77160344+SnazzyDucc@users.noreply.github.com>
2021-01-11 10:17:06 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+random = randint(1, 2)
+
+item = 0
+
+def on_button_pressed_a():
+    item == 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+    
+def on_button_pressed_b():
+    item == 2
+input.on_button_pressed(Button.B, on_button_pressed_b)
+    
+for i in range(5):
+    if random == item:
+        basic.show_string("Win!")
+    else:
+        basic.show_string("Lose")
+
+    
--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function on_forever() {
-    
+let random = randint(1, 2)
+let item = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    item == 1
 })
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    item == 2
+})
+for (let i = 0; i < 5; i++) {
+    if (random == item) {
+        basic.showString("Win!")
+    } else {
+        basic.showString("Lose")
+    }
+    
+}
